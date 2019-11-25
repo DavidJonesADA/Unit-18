bc891dcf80892cfdd740aac045a2380b39f16079
DavidJonesADA <david.jones@ada.ac.uk>
2019-11-25 12:49:56 +0000

    I have resized the image to half the size, as well as converting it to a .gif because the .png does not. As well as  creating the interface design for the new customer window so the staff at Silver Dawn Coaches cann enter the information, next i will add the SQL

--- main.py
+++ main.py
@@ -1,33 +1,108 @@
 from guizero import *
 import sqlite3
 
-x= 0
 
 # with sqlite3.connect("Unit 18") as db:
    #cursor = db.cursor()
 
 
-app = App(title='Silver Dawn Coaches', width=500, height=500)
+app = App(title='Silver Dawn Coaches', width=500, height=500, bg = (253, 71, 74))
 
-def open_splash_screen():
-       splash_window = Window(app, title = "Welcome to SD Coaches")
-       Text(splash_window, text="This is where you can Access the Database")
+def new_customer():
+          customer_window = Window(app, title = "Add Customer", bg = (253, 71, 74), width = 500, height = 800)
+          picture = Picture(customer_window, image="Logo.gif")
+
+          text = Text(customer_window, text= "New Customer")
+          text.text_color = "white"
+
+          text = Text(customer_window, text= "First Name")
+          text.text_color = "white"
+          first_name = TextBox(customer_window)
+          first_name.width = 15
+
+          text = Text(customer_window, text= "Last Name")
+          text.text_color = "white"
+          last_name = TextBox(customer_window)
+          last_name.width = 15
 
 
-def open_modify_screen():
-   modify_window = Window (app, title = "Modify the database")
-   Text(modify_window, text="This is where you can Modify the Database")
+          text = Text(customer_window, text= "Address 1")
+          text.text_color = "white"
+          address1 = TextBox(customer_window)
+          address1.width = 40
+
+          text = Text(customer_window, text= "Address 2")
+          text.text_color = "white"
+          address2 = TextBox(customer_window)
+          address2.width = 40
+
+          text = Text(customer_window, text= "Post Code")
+          text.text_color = "white"
+          post_code = TextBox(customer_window)
+          post_code.width = 15
+
+          text = Text(customer_window, text= "Phone Number")
+          text.text_color = "white"
+          phone_number = TextBox(customer_window)
+          phone_number.width = 25
+
+          text = Text(customer_window, text= "Email")
+          text.text_color = "white"
+          phone_number = TextBox(customer_window)
+          phone_number.width = 30
+
+          text = Text(customer_window, text= "Special Requirements?")
+
+
+
+
+
+
+
+def new_booking():
+          booking_window = Window (app, title = "Add Booking", bg = (253, 71, 74))
+          picture = Picture(booking_window, image="Logo.gif")
+          text = Text(booking_window, text="New Booking")
+          text.text_color = "white"
+
+def new_trip():
+          trip_window = Window (app, title = "Add Trip", bg = (253, 71, 74))
+          picture = Picture(app, image="Logo.gif")
+          text = Text(trip_window, text="New Trip")
+          text.text_color = "white"
+
+def new_destination():
+          destination_window = Window(app, title = "Add Destination", bg = (253, 71, 74))
+          picture = Picture(destination_window, image="Logo.gif")
+          text = Text(destination_window, text="New Destination")
+          text.text_color = "white"
+
+def query_data():
+          query_window = Window (app, title = "Query Data", bg = (253, 71, 74))
+          picture = Picture(query_window, image="Logo.gif")
+          text = Text(query_window, text="Query Data")
+          text.text_color = "white"
+
+
 
 text = Text(app, text="Silver Dawn Coaches Customer Software")
+text.text_color = "white"
 
-image = Text(app, image="Logo.png")
 
 
-submit = PushButton(app, text="Add Customer", command=open_splash_screen)
-submit = PushButton(app, text="Book Coach", command=open_modify_screen)
-submit = PushButton(app, text="Add Trip", command=open_modify_screen)
-submit = PushButton(app, text="Add Destination", command=open_modify_screen)
-submit = PushButton(app, text="Query Database", command=open_modify_screen)
+picture = Picture(app, image="Logo.gif")
+
+
+submit = PushButton(app, text="Add Customer", command=new_customer)
+submit.text_color = "white"
+submit = PushButton(app, text="Book Coach", command=new_booking)
+submit.text_color = "white"
+submit = PushButton(app, text="Add Trip", command=new_trip)
+submit.text_color = "white"
+submit = PushButton(app, text="Add Destination", command=new_destination)
+submit.text_color = "white"
+submit = PushButton(app, text="Query Database", command=query_data)
+submit.text_color = "white"
 
 
 
